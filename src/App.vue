2689b34491f9b46a57fe2c589c2aeffcdf85c8bf
Vopaaz <liyifan945@163.com>
2020-06-07 19:36:04 +0800

    QR page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img id="bg-img" src="./assets/qrcode.png" />
+    <span id="time-str">{{ timeStr }}</span>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {},
+  data: function() {
+    return {
+      timeStr: undefined
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      const date = new Date();
+      this.timeStr = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    });
   }
-}
+};
 </script>
 
 <style>
@@ -23,6 +30,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
+}
+
+#bg-img {
+  margin: 0px;
+  width: 100%;
+}
+
+#time-str{
+  position: absolute;
+  left: 28%;
+  top: 53%;
+  color: grey;
+  font-size: 21px;
 }
 </style>
