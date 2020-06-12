9ac54df654d1833f5f66947c54cbb0f0c241e308
Vopaaz <liyifan945@163.com>
2020-06-12 13:23:22 +0800

    Minor fix

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <div v-if="atQrPage">
-      <img id="bg-img" src="./assets/main.png" @click="toOutgoingPage" />
+      <img class="bg-img" src="./assets/main.png" @click="toOutgoingPage" />
       <span class="time-str main" id="time-str">{{ timeStr }}</span>
     </div>
     <div v-else>
-      <img id="bg-img-outgoing" src="./assets/outgoing.png" @click="toQrPage" />
+      <img class="bg-img" src="./assets/outgoing.png" @click="toQrPage" />
       <span class="time-str outgoing" id="time-str">{{ timeStr }}</span>
     </div>
   </div>
@@ -61,20 +61,7 @@ export default {
   margin: 0px;
 }
 
-#bg-img {
-  min-height: 100%;
-
-  /* Set up proportionate scaling */
-  width: 100%;
-  height: auto;
-
-  /* Set up positioning */
-  position: fixed;
-  top: 0;
-  left: 0;
-}
-
-#bg-img-outgoing {
+.bg-img {
   min-height: 100%;
 
   /* Set up proportionate scaling */
@@ -96,14 +83,14 @@ export default {
 
 .main {
   /* For iphone, confirmed */
-  /* top: 51.7%; */
+  /* top: 51.2%; */
   /* For xiaomi, confirmed */
   top: 62%;
 }
 
 .outgoing {
   /* For iphone, confirmed */
-  /* top: 51%; */
+  /* top: 50.9%; */
   /* For xiaomi, confirmed */
   top: 61.85%;
 }
