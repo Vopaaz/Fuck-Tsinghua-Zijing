00d3ad542d7759573d394688c33f59e487895213
Vopaaz <liyifan945@163.com>
2020-07-25 14:38:34 +0800

    Adjust iPhone layout

--- src/App.vue
+++ src/App.vue
@@ -91,14 +91,14 @@ export default {
 
 .main {
   /* For iphone, confirmed */
-  top: 49.9%;
+  top: 49.8%;
   /* For xiaomi, confirmed */
   /* top: 57.5%; */
 }
 
 .outgoing {
   /* For iphone, confirmed */
-  top: 49.3%;
+  top: 49.2%;
   /* For xiaomi, confirmed */
   /* top: 56.7%; */
 }
